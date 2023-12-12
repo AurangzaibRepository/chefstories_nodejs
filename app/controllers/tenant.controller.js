975771f0a6c94c697f4f67964dcb09de1b69925f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-12 20:33:31 +0500

    Implements add action

--- app/controllers/tenant.controller.js
+++ app/controllers/tenant.controller.js
@@ -2,15 +2,20 @@ const requestHelper = require("../utils/request.helper");
 
 exports.listing = (req, res) => {
   try {
-    return res.send(requestHelper.getResponse(
-      true,
-      "Listing goes here",
-    ));
+    return res.status(200)
+      .send(requestHelper.getResponse(true, "Listing goes here"));
   } catch (exception) {
-    return res.send(requestHelper.getResponse(
-      false,
-      null,
-      exception.message,
-    ));
+    return res.status(500)
+      .send(requestHelper.getResponse(false, null, exception.message));
+  }
+};
+
+exports.add = (req, res) => {
+  try {
+    return res.status(200)
+      .send(requestHelper.getResponse(true, "Tenant added successfully"));
+  } catch (exception) {
+    return res.status(500)
+      .send(requestHelper.getResponse(false, null, exception.message));
   }
 };
