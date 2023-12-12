0551a5ed4ba469ea80e31fec9792dc33c35e5ee0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-12 21:03:02 +0500

    Defines route for all tenants

--- app/routes/tenant.route.js
+++ app/routes/tenant.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const tenants = require("../controllers/tenant.controller");
 
 module.exports = (app) => {
+  router.get("/all", tenants.all);
   router.get("/:pageNumber/:keyword?", tenants.listing);
   router.post("/", tenants.add);
   router.put("/:id", tenants.update);
