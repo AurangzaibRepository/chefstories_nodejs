5c21729c4a2ce80e3733c5f3650781fc9e04022d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-13 00:38:54 +0500

    Includes upload middleware in add route

--- app/routes/tenant.route.js
+++ app/routes/tenant.route.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const tenants = require("../controllers/tenant.controller");
+const { upload } = require("../middlewares/upload.middleware");
 
 module.exports = (app) => {
   router.get("/all", tenants.all);
   router.get("/:pageNumber/:keyword?", tenants.listing);
-  router.post("/", tenants.add);
+  router.post("/", upload("tenants").single("logo"), tenants.add);
   router.put("/:id", tenants.update);
 
   app.use("/api/tenants", router);
