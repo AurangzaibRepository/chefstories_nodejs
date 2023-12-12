c9ae9afdf67230959a5849ecca2b905869f1cb42
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-12 20:34:45 +0500

    Defines route for add tenant

--- app/routes/tenant.route.js
+++ app/routes/tenant.route.js
@@ -3,6 +3,7 @@ const tenants = require("../controllers/tenant.controller");
 
 module.exports = (app) => {
   router.get("/:pageNumber/:keyword?", tenants.listing);
+  router.post("/", tenants.add);
 
   app.use("/api/tenants", router);
 };
