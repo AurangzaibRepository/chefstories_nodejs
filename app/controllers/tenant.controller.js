188c657333f7529b2a78d5a22fd3882748318518
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-12 21:00:25 +0500

    Implements update function

--- app/controllers/tenant.controller.js
+++ app/controllers/tenant.controller.js
@@ -1,3 +1,4 @@
+const tenantService = require("../services/tenant.service");
 const requestHelper = require("../utils/request.helper");
 
 exports.listing = (req, res) => {
@@ -10,10 +11,24 @@ exports.listing = (req, res) => {
   }
 };
 
-exports.add = (req, res) => {
+exports.add = async (req, res) => {
   try {
+    await tenantService.add(req.body);
+
     return res.status(200)
       .send(requestHelper.getResponse(true, "Tenant added successfully"));
+  } catch (exception) {
+    return res.status(500)
+      .send(requestHelper.getResponse(false, null, exception.message));
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    await tenantService.update(req.params.id, req.body);
+
+    return res.status(200)
+      .send(requestHelper.getResponse(true, "Tenant updated successfully"));
   } catch (exception) {
     return res.status(500)
       .send(requestHelper.getResponse(false, null, exception.message));
