c554ee34f24774d6bc836f67aaff0f02daaa86be
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-12 20:59:53 +0500

    Defines route for update tenant

--- app/routes/tenant.route.js
+++ app/routes/tenant.route.js
@@ -4,6 +4,7 @@ const tenants = require("../controllers/tenant.controller");
 module.exports = (app) => {
   router.get("/:pageNumber/:keyword?", tenants.listing);
   router.post("/", tenants.add);
+  router.put("/:id", tenants.update);
 
   app.use("/api/tenants", router);
 };
