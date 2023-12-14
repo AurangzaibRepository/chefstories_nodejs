11e1cf5aedf77f489f22c0090d7d1d4c26bdca92
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-14 21:10:04 +0500

    Defines route for user

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -3,6 +3,9 @@ const users = require("../controllers/user.controller");
 
 module.exports = (app) => {
   router.get("/all/:tenantId?", users.all);
+  router.get("/:pageNumber/:keyword?", users.listing);
+  router.post("/", users.add);
+  router.put("/:id", users.update);
 
   app.use("/api/users", router);
 };
