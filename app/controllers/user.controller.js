23b44ee516e5f72a460499cf88599d125a3b0401
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-14 19:07:57 +0500

    Creates controller for user

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,14 @@
+const userService = require("../services/user.service");
+const requestHelper = require("../utils/request.helper");
+
+exports.all = async (req, res) => {
+  try {
+    const data = await userService.getAll();
+
+    return res.status(200)
+      .send(requestHelper.getResponse(true, null, null, data));
+  } catch (exception) {
+    return res.status(500)
+      .send(requestHelper.getResponse(false, null, exception.message));
+  }
+};
