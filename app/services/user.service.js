95f4ff4856a0e385fc46fb35e725f6311fc2c4fe
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-14 21:01:32 +0500

    Implements where condition based on paramater

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,7 +1,14 @@
 const db = require("../models");
 
-exports.getAll = async () => {
+exports.getAll = async (tenantId) => {
+  const condition = {};
+
+  if (tenantId) {
+    condition.tenant_id = tenantId;
+  }
+
   const data = await db.users.findAll({
+    where: condition,
     attributes: ["id", "first_name"],
     order: [
       ["id", "DESC"],
