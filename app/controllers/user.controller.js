692d21e34d297105d6ac823556b609915bc237aa
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-16 15:21:55 +0500

    Calls service function to get listing

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -13,8 +13,16 @@ exports.all = async (req, res) => {
   }
 };
 
-exports.listing = (req, res) => {
+exports.listing = async (req, res) => {
+  try {
+    const data = await userService.getListing();
 
+    return res.status(200)
+      .send(requestHelper.getResponse(true, null, null, data));
+  } catch (exception) {
+    return res.status(500)
+      .send(requestHelper.getResponse(false, null, exception.message));
+  }
 };
 
 exports.add = async (req, res) => {
