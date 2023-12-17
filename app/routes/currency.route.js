0aaa3dc5b2de8369ff89876afb83448aaacc014e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-17 14:49:10 +0500

    Defien rouet for get

--- app/routes/currency.route.js
+++ app/routes/currency.route.js
@@ -3,6 +3,7 @@ const currencies = require("../controllers/currency.controller");
 
 module.exports = (app) => {
   router.get("/all/:tenantId", currencies.all);
+  router.get("/:id", currencies.get);
 
   app.use("/api/currencies", router);
 };
