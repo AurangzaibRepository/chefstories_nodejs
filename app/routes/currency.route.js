cf6e1c5fc3bcbd0f7bf9aae1e6916220778d0ceb
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-17 15:32:00 +0500

    Specifies listing callback frunction

--- app/routes/currency.route.js
+++ app/routes/currency.route.js
@@ -3,7 +3,7 @@ const currencies = require("../controllers/currency.controller");
 
 module.exports = (app) => {
   router.get("/all/:tenantId", currencies.all);
-  router.get("/listing/:pageNumber/:keyword?");
+  router.get("/listing/:pageNumber/:keyword?", currencies.listing);
   router.get("/:id", currencies.get);
   router.post("/", currencies.add);
   router.put("/:id", currencies.update);
