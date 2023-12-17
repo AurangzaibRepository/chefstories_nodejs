5e362d75d5764d31a92abc390558c2df66f51a4f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-17 14:23:19 +0500

    Amend callback name

--- app/routes/currency.route.js
+++ app/routes/currency.route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const currencies = require("../controllers/currency.controller");
 
 module.exports = (app) => {
-  router.get("/all/:tenantId?", currencies.getAll);
+  router.get("/all/:tenantId?", currencies.all);
 
   app.use("/api/currencies", router);
 };
