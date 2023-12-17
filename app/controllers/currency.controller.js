19f1db0743bc548a3f741bc195b89d216e7392af
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-17 14:51:53 +0500

    Implements function for get

--- app/controllers/currency.controller.js
+++ app/controllers/currency.controller.js
@@ -12,3 +12,15 @@ exports.all = async (req, res) => {
       .send(requestHelper.getResponse(false, null, exception.message));
   }
 };
+
+exports.get = async (req, res) => {
+  try {
+    const data = await currencyService.get(req.params.id);
+
+    return res.status(200)
+      .send(requestHelper.getResponse(true, null, null, data));
+  } catch (exception) {
+    return res.status(500)
+      .send(requestHelper.getResponse(false, null, exception.message));
+  }
+};
