2441bdc0a183b11a5c49c54ae931b4fb688987b7
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-19 21:19:53 +0500

    Implements error handlign via next

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,62 +1,67 @@
 const userService = require("../services/user.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.all = async (req, res) => {
+exports.all = async (req, res, next) => {
   try {
     const data = await userService.getAll(req.params.tenantId);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.listing = async (req, res) => {
+exports.listing = async (req, res, next) => {
   try {
     const data = await userService.getListing(req.params);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.get = async (req, res) => {
+exports.get = async (req, res, next) => {
   try {
     const data = await userService.get(req.params.id);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.add = async (req, res) => {
+exports.add = async (req, res, next) => {
   try {
     await userService.add(req.body);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, "User added successfully"));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.update = async (req, res) => {
+exports.update = async (req, res, next) => {
   try {
     await userService.update(req.params.id, req.body);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, "User updated successfully"));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
