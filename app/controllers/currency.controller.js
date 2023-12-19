230a267fa660ebd897af82cc9e7cb382b4a65b53
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-19 21:01:18 +0500

    Amends controller to implement error via next

--- app/controllers/currency.controller.js
+++ app/controllers/currency.controller.js
@@ -1,62 +1,67 @@
 const requestHelper = require("../utils/request.helper");
 const currencyService = require("../services/currency.service");
 
-exports.all = async (req, res) => {
+exports.all = async (req, res, next) => {
   try {
     const data = await currencyService.getAll(req.params.tenantId);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.listing = async (req, res) => {
+exports.listing = async (req, res, next) => {
   try {
     const data = await currencyService.getListing(req.params);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.get = async (req, res) => {
+exports.get = async (req, res, next) => {
   try {
     const data = await currencyService.get(req.params.id);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.add = async (req, res) => {
+exports.add = async (req, res, next) => {
   try {
     await currencyService.add(req.body);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, "Currency added successfully"));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.update = async (req, res) => {
+exports.update = async (req, res, next) => {
   try {
     await currencyService.update(req.params.id, req.body);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, "Currency updated successfully"));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
