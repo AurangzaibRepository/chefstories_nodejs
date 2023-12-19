467cd997a1dab49f4432dd6f50dbed3841641cd3
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-19 21:33:56 +0500

    Defines defautlscope

--- app/models/language.model.js
+++ app/models/language.model.js
@@ -25,6 +25,11 @@ module.exports = (sequelize, Sequelize) => {
   }, {
     createdAt: "created_at",
     updatedAt: "updated_at",
+    defaultScope: {
+      attributes: {
+        exclude: ["created_at", "updated_at"],
+      },
+    },
   });
 
   return Language;
