6475b32406f7728b39c1ec9d1f4e3c7c94d00120
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-19 19:46:11 +0500

    Craetes helper for currency

---
+++ app/utils/currency.helper.js
@@ -0,0 +1,30 @@
+const db = require("../models");
+
+const { Op } = db.Sequelize;
+
+exports.prepareCondition = (parameters) => {
+  let condition = {
+    tenant_id: parameters.tenantId,
+  };
+
+  if (parameters.keyword) {
+    const keywordCondition = {
+      [Op.or]: [
+        {
+          name: {
+            [Op.substring]: parameters.keyword,
+          },
+        },
+        {
+          short_name: {
+            [Op.substring]: parameters.keyword,
+          },
+        },
+      ],
+    };
+
+    condition = { ...condition, ...keywordCondition };
+  }
+
+  return condition;
+};
