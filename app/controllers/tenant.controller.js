d2a410249948dfa7b9c5ee936756cf5c554a040e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-19 21:14:18 +0500

    Implements erro handling via next

--- app/controllers/tenant.controller.js
+++ app/controllers/tenant.controller.js
@@ -1,62 +1,67 @@
 const tenantService = require("../services/tenant.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.all = async (req, res) => {
+exports.all = async (req, res, next) => {
   try {
     const data = await tenantService.getAll();
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.listing = async (req, res) => {
+exports.listing = async (req, res, next) => {
   try {
     const data = await tenantService.getListing(req.params);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.get = async (req, res) => {
+exports.get = async (req, res, next) => {
   try {
     const data = await tenantService.get(req.params.id);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, null, null, data));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.add = async (req, res) => {
+exports.add = async (req, res, next) => {
   try {
     await tenantService.add(req.body, req.file);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, "Tenant added successfully"));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
 
-exports.update = async (req, res) => {
+exports.update = async (req, res, next) => {
   try {
     await tenantService.update(req.params.id, req.body, req.file);
 
     return res.status(200)
       .send(requestHelper.getResponse(true, "Tenant updated successfully"));
   } catch (exception) {
-    return res.status(500)
-      .send(requestHelper.getResponse(false, null, exception.message));
+    next(exception.message);
+
+    return false;
   }
 };
