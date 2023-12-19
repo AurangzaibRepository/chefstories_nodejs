92de07e2be0c5f26a7af562a23ebded2d47979f0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-19 22:00:38 +0500

    Implements listing action

--- app/controllers/language.controller.js
+++ app/controllers/language.controller.js
@@ -10,3 +10,14 @@ exports.all = (req, res, next) => {
     return false;
   }
 };
+
+exports.listing = (req, res, next) => {
+  try {
+    return res.status(200)
+      .send(requestHelper.getResponse(true));
+  } catch (exception) {
+    next(exception.message);
+
+    return false;
+  }
+};
