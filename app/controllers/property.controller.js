c04065b19a030ad02858c2d56b05c8b6ee94f75c
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-23 21:51:05 +0500

    Removes listing action

--- app/controllers/property.controller.js
+++ app/controllers/property.controller.js
@@ -10,16 +10,6 @@ exports.all = (req, res, next) => {
   }
 };
 
-exports.listing = (req, res, next) => {
-  try {
-    return res.send(requestHelper.getResponse(true));
-  } catch (exception) {
-    next(exception.message);
-
-    return false;
-  }
-};
-
 exports.get = (req, res, next) => {
   try {
     return res.send(requestHelper.getResponse(true));
