4a80025c110727c01b0b910f9bff8078da80bbbe
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-23 19:26:05 +0500

    Defines columns

--- app/models/propertyValue.model.js
+++ app/models/propertyValue.model.js
@@ -1,8 +1,19 @@
 module.exports = (sequelize, Sequelize) => {
   const propertyValue = sequelize.define("propertyValue", {
-
+    name: {
+      type: Sequelize.STRING(70),
+      allowNull: false,
+    },
+    position: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    created_at: Sequelize.DATEONLY,
+    updated_at: Sequelize.DATEONLY,
   }, {
     tableName: "property_values",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
   });
 
   return propertyValue;
