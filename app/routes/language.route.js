da45be77044d860aee7e521ac66dc8df0961d6a1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-23 14:32:42 +0500

    Defines et route

--- app/routes/language.route.js
+++ app/routes/language.route.js
@@ -4,6 +4,7 @@ const languages = require("../controllers/language.controller");
 module.exports = (app) => {
   router.get("/all", languages.all);
   router.get("/listing/:pageNumber/tenantId/:keyword?", languages.listing);
+  router.get("/:id", languages.get);
   router.post("/", languages.add);
   router.put("/:id", languages.update);
 
