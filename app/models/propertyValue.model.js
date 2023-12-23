4b92e3a6c1c087cc23ec34ac226fb6ab62aee97e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-23 20:35:13 +0500

    Defines default scope

--- app/models/propertyValue.model.js
+++ app/models/propertyValue.model.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, Sequelize) => {
     tableName: "property_values",
     createdAt: "created_at",
     updatedAt: "updated_at",
+    defaultScope: {
+      attributes: {
+        exclude: ["created_at", "updated_at"],
+      },
+    },
   });
 
   return propertyValue;
