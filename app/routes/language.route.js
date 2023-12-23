36e6209a3e7681d829897d3a54ed7665a6934c85
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-23 14:49:13 +0500

    Adds paarmeter

--- app/routes/language.route.js
+++ app/routes/language.route.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const languages = require("../controllers/language.controller");
 
 module.exports = (app) => {
-  router.get("/all", languages.all);
-  router.get("/listing/:pageNumber/tenantId/:keyword?", languages.listing);
+  router.get("/all/:tenantId", languages.all);
+  router.get("/listing/:pageNumber/:tenantId/:keyword?", languages.listing);
   router.get("/:id", languages.get);
   router.post("/", languages.add);
   router.put("/:id", languages.update);
