7ab75baef3e63301e8854ab3910690cdc9d7a3e8
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-23 18:40:53 +0500

    Impleemnts service function

--- app/services/language.service.js
+++ app/services/language.service.js
@@ -1,4 +1,6 @@
 const db = require("../models");
+const formatHelper = require("../utils/format.helper");
+const languageHelper = require("../utils/language.helper");
 
 exports.getAll = async (tenantId) => {
   const data = await db.languages.scope("orderName").findAll({
@@ -9,6 +11,21 @@ exports.getAll = async (tenantId) => {
   });
 
   return data;
+};
+
+exports.getListing = async (parameters) => {
+  const pageSize = parseInt(process.env.PAGE_SIZE, 10);
+  const offset = (parameters.pageNumber * pageSize) - pageSize;
+  const condition = languageHelper.prepareCondition(parameters);
+  const recordCount = await db.languages.count({where: condition});
+
+  const data = await db.languages.scope("defaultScope", "orderLatest", "limit")
+    .findAll({
+      where: condition,
+      offset,
+    });
+
+  return formatHelper.formatListing(parameters.pageNumber, recordCount, data);
 };
 
 exports.get = async (id) => {
