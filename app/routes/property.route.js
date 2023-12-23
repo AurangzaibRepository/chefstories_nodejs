c4f43a0c2e0071054eceae285c0fab70e8f8dbed
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-12-23 21:50:29 +0500

    Defines get route for proeprties

--- app/routes/property.route.js
+++ app/routes/property.route.js
@@ -3,6 +3,7 @@ const properties = require("../controllers/property.controller");
 
 module.exports = (app) => {
   router.get("/all/:tenantId", properties.all);
+  router.get("/:id", properties.get);
   router.post("/", properties.add);
   router.put("/:id", properties.update);
 
